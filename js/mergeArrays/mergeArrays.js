2dd01b510db573ed5fc4f62545ca0356789063f2
Katerina Tuma <1katyakaterina@gmail.com>
2024-08-12 21:34:35 +0300

    Refactored mergeArrays

--- js/mergeArrays/mergeArrays.js
+++ js/mergeArrays/mergeArrays.js
@@ -6,7 +6,8 @@
  */
 
 export const mergeArrays = (arr1, arr2) => {
-  const mergedAndSortedArr = [...arr1, ...arr2].sort((num1, num2) => num1 - num2);
-  const uniqueNumSet = new Set(mergedAndSortedArr);
-  return [...uniqueNumSet];
+  const sortedList = [...arr1, ...arr2]
+    .sort((num1, num2) => num1 - num2);
+  const uniqueList = new Set(sortedList);
+  return [...uniqueList];
 };
