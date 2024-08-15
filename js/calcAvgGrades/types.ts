3f8163b39d81cb064baab9fafa621ab29f549823
Katerina Tuma <1katyakaterina@gmail.com>
2024-08-15 22:30:45 +0300

    Added types for tests

--- js/calcAvgGrades/types.ts
+++ js/calcAvgGrades/types.ts
@@ -8,3 +8,14 @@ export type StudentsAvgGrades = {
   lastName: string;
   avgGrade: number;
 };
+
+type TestUnit = {
+  students: Students[];
+  expectedStudentsAvgGrades: StudentsAvgGrades[];
+};
+
+export type TestData = {
+  test1: TestUnit;
+  test2: TestUnit;
+  test3: TestUnit;
+};
