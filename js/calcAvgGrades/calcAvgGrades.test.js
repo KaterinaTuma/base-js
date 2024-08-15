8986c716d49f289d99c2f9247c77eddfde94e431
Katerina Tuma <1katyakaterina@gmail.com>
2024-08-15 21:49:07 +0300

    Refactored names in tests

--- js/calcAvgGrades/calcAvgGrades.test.js
+++ js/calcAvgGrades/calcAvgGrades.test.js
@@ -7,7 +7,7 @@ const testData = {
       { firstName: 'Petr', lastName: 'Petrov', grades: [5, 5, 4] },
       { firstName: 'Sidor', lastName: 'Sidorov', grades: [2, 3, 3] },
     ],
-    expectedStudentAvgGrades: [
+    expectedStudentsAvgGrades: [
       { lastName: 'Ivanov', avgGrade: 4 },
       { lastName: 'Petrov', avgGrade: 4.67 },
       { lastName: 'Sidorov', avgGrade: 2.67 },
@@ -19,7 +19,7 @@ const testData = {
       { firstName: 'Anna', lastName: 'Smirnova', grades: [5, 4, 4, 5] },
       { firstName: 'Olga', lastName: 'Kuznetsova', grades: [3, 3, 4, 4] },
     ],
-    expectedStudentAvgGrades: [
+    expectedStudentsAvgGrades: [
       { lastName: 'Smirnova', avgGrade: 4.5 },
       { lastName: 'Kuznetsova', avgGrade: 3.5 },
     ],
@@ -29,7 +29,7 @@ const testData = {
     arr: [
       { firstName: 'Elena', lastName: 'Alexandrova', grades: [3, 3, 3] },
     ],
-    expectedStudentAvgGrades: [
+    expectedStudentsAvgGrades: [
       { lastName: 'Alexandrova', avgGrade: 3 },
     ],
   },
@@ -39,18 +39,18 @@ describe('calcAvg', () => {
   test('Unit test1', () => {
     const test1 = testData.test1;
     const studentAvgGrades = calcAvgCrades(test1.arr);
-    expect(studentAvgGrades).toEqual(test1.expectedStudentAvgGrades);
+    expect(studentAvgGrades).toEqual(test1.expectedStudentsAvgGrades);
   });
 
   test('Unit test2', () => {
     const test2 = testData.test2;
     const studentAvgGrades = calcAvgCrades(test2.arr);
-    expect(studentAvgGrades).toEqual(test2.expectedStudentAvgGrades);
+    expect(studentAvgGrades).toEqual(test2.expectedStudentsAvgGrades);
   });
 
   test('Unit test3', () => {
     const test3 = testData.test3;
     const studentAvgGrades = calcAvgCrades(test3.arr);
-    expect(studentAvgGrades).toEqual(test3.expectedStudentAvgGrades);
+    expect(studentAvgGrades).toEqual(test3.expectedStudentsAvgGrades);
   });
 });
